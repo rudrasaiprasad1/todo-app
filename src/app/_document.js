73c98c982da598c73e579fa283a47d65ded40de5
saiprasad <saiprasadrudra8@gmail.com>
2025-06-14 23:30:32 +0530

    first commit

---
+++ src/app/_document.js
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:wght@100;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+          crossOrigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <div id="portal"></div>
+        <div id="portal2"></div>
+      </body>
+    </Html>
+  );
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Layout from "../../components/Layout";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,10 +20,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <Layout>{children}</Layout>;
       </body>
     </html>
   );
