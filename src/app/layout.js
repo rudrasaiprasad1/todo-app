4d2283b758097c0a5d517e0808094fe374fc9f4c
saiprasad <saiprasadrudra8@gmail.com>
2025-06-16 09:15:19 +0530

    updated the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import Layout from "../../components/Layout";
+import Layout from "@/components/Layout.jsx";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +20,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Layout>{children}</Layout>;
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:wght@100;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <body className="flex flex-col  min-h-screen relative bg-slate-900">
+        <div className="flex-1">{children}</div>
+        <div id="portal"></div>
+        <div id="portal2"></div>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import { Login } from "./../../components/Login.jsx";
 export default function Home() {
   return (
-    <>
+    <div>
       <div>Home page</div>
-      <i className="fa-solid fa-house"></i>
-      <Login />
-    </>
+      <div className="w-24 h-24 rounded-xl border-4 border-green-500  bg-white dark:bg-black">
+        <i
+          height="24px"
+          width="24px"
+          className="w-24 h-24 fa-solid fa-house dark:text-white text-black"
+        ></i>
+      </div>
+      <i className="fa-solid fa-house w-24 h-24 rounded-xl border-4 border-green-500  bg-white dark:bg-black"></i>
+    </div>
   );
 }
