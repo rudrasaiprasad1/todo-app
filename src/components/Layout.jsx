e6c45ad8bc01b4e9aed885724c241cc07e4faa82
saiprasad <saiprasadrudra8@gmail.com>
2025-06-16 23:13:53 +0530

    updated the layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
-const Layout = (props) => {
+function Layout(props) {
   const { children } = props;
   return (
     <div className="flex flex-col  min-h-screen relative bg-slate-900 ">
@@ -10,6 +10,6 @@ const Layout = (props) => {
       <Footer />
     </div>
   );
-};
+}
 
 export default Layout;
