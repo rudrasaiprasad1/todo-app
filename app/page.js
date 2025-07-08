12f07c3dc44dc9b6942a678593765311a01e03f1
rudrasaiprasad1 <saiprasadrudra10@gmail.com>
2025-07-09 00:15:48 +0530

    homepage component updated

--- app/page.js
+++ app/page.js
@@ -9,7 +9,6 @@ export default function Home() {
           className="w-24 h-24 fa-solid fa-house dark:text-white text-black"
         ></i>
       </div>
-      <i className="fa-solid fa-house w-24 h-24 rounded-xl border-4 border-green-500  bg-white dark:bg-black"></i>
     </div>
   );
 }
