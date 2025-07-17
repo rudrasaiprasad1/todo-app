a4b987eb7f53d7894edcc8f1237ac96b0e5e175a
rudrasaiprasad1 <saiprasadrudra10@gmail.com>
2025-07-17 23:58:48 +0530

    Header component  updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <>
       <div className="sticky top-0 w-full left-0 bg-green-300 text-green-700 dark:text-green-700 flex justify-between items-center p-4 px-6 border-b border-white">
         <h1 className="text-3xl sm:text-6xl select-none">TODO liST</h1>
-        <i className="fa-solid fa-user text-xl sm:text-3xl"></i>
+        <i className="fa-solid fa-user text-xl sm:text-3xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
       </div>
     </>
   );
