9881ee729dc7593a71e262723d899e219501fc1b
Rudra Saiprasad <130814487+rudrasaiprasad1@users.noreply.github.com>
2025-07-23 14:58:20 +0530

    Update layout.js

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "TodoApp",
+  title: "Todo App",
   description: `Created with 💖 by rsp `,
 };
 
