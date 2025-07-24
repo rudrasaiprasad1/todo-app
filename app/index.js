6b33a675b123b1d3f73e48f8e765ac5c2fd1c5cc
rudrasaiprasad1 <saiprasadrudra10@gmail.com>
2025-07-25 00:05:59 +0530

    Header layout and login  component  updated

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <div className="flex justify-center align-middle">
+        <div className="text-white dark:text-shadow-indigo-400">Home</div>
+      </div>
+    </>
+  );
+};
+export default Home;
