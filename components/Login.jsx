2c34b033a0dd4862e0dabab9108ab180ba558842
rudrasaiprasad1 <saiprasadrudra10@gmail.com>
2025-07-27 20:48:32 +0530

    Header layout and login  component  updated

--- components/Login.jsx
+++ components/Login.jsx
@@ -2,11 +2,14 @@ export const Login = () => {
   return (
     <div className="flex flex-1 flex-col text-xs sm:text-sm justify-center items-center bg-green-300 ">
       <h1 className="font-extrabold text-2xl">Login</h1>
-      <input
-        type="text"
-        placeholder="Email address..."
-        className="outline-none text-slate-900 p-2 w-full max-w-[30ch]"
-      />
+      <form>
+        <input
+          type="text"
+          placeholder="Email address..."
+          className="outline-none text-slate-900 p-2 w-full max-w-[30ch]"
+        />
+        <button />
+      </form>
     </div>
   );
 };
