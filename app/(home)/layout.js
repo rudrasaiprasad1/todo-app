c2217a443c98086e932e9bdb624a7d4ab60976d1
rudrasaiprasad1 <saiprasadrudra10@gmail.com>
2025-08-18 23:30:37 +0530

    global.css updated for tailwindcss

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <CDNLoader />
-      <body className="flex flex-col min-h-screen relative bg-slate-900">
+      <body className="flex flex-col min-h-screen relative dark:bg-slate-900 bg-amber-50">
         <main>
           <div className="flex-1">{children}</div>
         </main>
