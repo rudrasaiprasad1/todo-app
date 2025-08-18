3b3e6964cc4d557602e9df0e00e415b9d73922e6
rudrasaiprasad1 <saiprasadrudra10@gmail.com>
2025-08-18 23:10:39 +0530

    global.css updated for tailwindcss

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,12 +1,12 @@
+import CDNLoader from "../components/CDNLoader";
 export const metadata = {
   title: "Todo App",
   description: `Created with 💖 by rsp `,
 };
-
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <Head />
+    <html lang="en">
+      <CDNLoader />
       <body className="flex flex-col min-h-screen relative bg-slate-900">
         <main>
           <div className="flex-1">{children}</div>
---
+++ app/components/CDNLoader.jsx
@@ -0,0 +1,27 @@
+"use client";
+import Head from "next/head";
+export default function CDNLoader() {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:wght@100;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+    </>
+  );
+}
